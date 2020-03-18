2e11d8a362275728627df012d38c6e7959cd8665
Tim López <timbislopez@gmail.com>
2020-03-17 23:16:15 -0300

    changes

--- modules/item/index.js
+++ modules/item/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 export const Item = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <div className='item__container'>
+      <div className={`item__image`}>
+        {tag && <div className='item__tag'>{tag}</div>}
+      </div>
+
+      <span className="item__name">{name}</span>
+
+      <div className="item__info-line">
+        {priceComparation && 
+          <span className="item__price-comparation">
+            ${priceComparation}
+          </span>}
+        
+        <span className="item__price">
+          ${price}
+        </span>
+        <a href={`/product/${id}`}>
+          <div className="item__more">Comprar</div>
+        </a>
+      </div>
+    </div>
+  );
+};
